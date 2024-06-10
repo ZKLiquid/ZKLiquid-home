248e239e8b728e768fdf248e702999f142f4a64b
SholaOti <shola.otitoju2018@gmail.com>
2024-06-10 11:04:25 +0300

    updates

--- src/components/Integration/video/Video.jsx
+++ src/components/Integration/video/Video.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function Video() {
   useEffect(() => {
     // Replace the current URL with the YouTube video link when the component mounts
-    window.location.replace("https://www.youtube.com/watch?v=J2MBV1fdUtE");
+    window.location.replace("https://www.youtube.com/watch?v=NR-JywFofY0");
   }, []);
 
   return <div>Video</div>;
